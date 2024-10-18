466d87382476b3c0453382a9e8d22b1aaa4c066d
Geancarllo Moraes <geancarllo.cm@gmail.com>
2024-10-18 18:08:07 -0400

    novas aulas

---
+++ aula11/ex008.js
@@ -0,0 +1,12 @@
+var vel = 50.5
+console.log(` A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
+
+/*É uma concição simples pois quando nao tem o else(se não) é configurado condição simples.
+
+Se você ultrapassou o limite de velocidade você ganha uma multa, se não ultrapassou não acontece nada.
+Concição simples*/
